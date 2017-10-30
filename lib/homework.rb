012f2697019105d1a45d05cdfea56dec2398d287
marizaswart <mariza.swart@gmail.com>
2017-10-30 22:25:58 +0100

    Homeworked pass rspec but not correct

---
+++ lib/homework.rb
@@ -0,0 +1,7 @@
+
+def product(array, multiplier)
+    array.each do |item|
+     print item * multiplier
+    end
+end
+product([1, 2, 3, 4], 2)
--- spec/homework_spec.rb
+++ spec/homework_spec.rb
@@ -1,5 +1,9 @@
-require '.lib/homework.rb'
+require './lib/homework'
 
 describe 'product' do
-  it 
+  it 'multiplies the array objects by 2' do
+    expect do
+      (product([1, 2, 3, 4], 2))
+    end.to output("2468").to_stdout
+end
 end
