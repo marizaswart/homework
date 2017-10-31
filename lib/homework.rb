7d2b4072e9e2ee6ea6ee76564310e3b9d1528821
marizaswart <mariza.swart@gmail.com>
2017-10-31 20:56:00 +0100

    Multiple versions of code to solve the assignment with the final solution in the final tests

--- lib/homework.rb
+++ lib/homework.rb
@@ -1,7 +1,25 @@
+=begin
+def product(array, multiplier)
+  array = [1, 2, 3, 4]
+    array.each { |number, n| number * n }
+end
+
+class Array
+end
 
 def product(array, multiplier)
-    array.each do |item|
-     print item * multiplier
-    end
+  array = []
+  array * multiplier == 0
 end
-product([1, 2, 3, 4], 2)
+def multiply_by_number(multiplier)
+  collect! { |number| number *  }
+end
+=end
+def product(array, multiplier)
+  #arr = []
+  array.each_with_index do |value, index|
+    array[index] << value * multiplier
+  #  arr << results
+  end
+#  arr
+end
--- spec/homework_spec.rb
+++ spec/homework_spec.rb
@@ -1,9 +1,12 @@
 require './lib/homework'
 
-describe 'product' do
-  it 'multiplies the array objects by 2' do
-    expect do
-      (product([1, 2, 3, 4], 2))
-    end.to output("2468").to_stdout
-end
+describe '#product' do
+  before do
+    @array = [1, 2, 3, 4]
+  end
+
+  it "returns the multiplied value of the objects in the array" do
+    product(@array, 2)
+    expect(@array).to eq [2, 4, 6, 8]
+  end
 end
